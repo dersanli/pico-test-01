59aef6ed6c43bf523a36a5c098215b1bcde11919
Devrim Ersanli <dersanli@gmail.com>
2026-04-12 19:53:58 +0100

    Add USB HID gamepad support with steering axis and engine button

--- boot.py
+++ boot.py
@@ -1,8 +1,2 @@
-import sys
-
-try:
-    import hid_gamepad
-    hid_gamepad.gamepad = hid_gamepad.init()
-except Exception as e:
-    with open('boot_error.log', 'w') as f:
-        sys.print_exception(e, f)
+import hid_gamepad
+hid_gamepad.gamepad = hid_gamepad.init()
